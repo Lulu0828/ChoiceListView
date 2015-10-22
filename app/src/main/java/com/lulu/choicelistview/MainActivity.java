b59ecd17782aa97105e72f442de703d60b783e7e
xylu <xylu@chinaums.com>
2015-10-22 15:48:04 +0800

    加载singlechoicelist

--- app/src/main/java/com/lulu/choicelistview/MainActivity.java
+++ app/src/main/java/com/lulu/choicelistview/MainActivity.java
@@ -4,13 +4,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> data = new ArrayList<>();
+
+    private ListView choiceListView;
+
+    private ListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 5; i ++) {
+            data.add("test" + i);
+        }
+
+        choiceListView = (ListView) findViewById(R.id.lv_singlechoice);
+        choiceListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        adapter = new ArrayAdapter<String>(this, R.layout.item_single_choice, data){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                final ChoiceView view;
+                if (convertView == null) {
+                    view = new ChoiceView(MainActivity.this);
+                } else {
+                    view = (ChoiceView) convertView;
+                }
+                view.setText(getItem(position));
+                return view;
+            }
+        };
+        choiceListView.setAdapter(adapter);
     }
 
     @Override
